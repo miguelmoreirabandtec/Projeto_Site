0b4cfc493829b1be6db24de56f12468531223010
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-10-25 15:17:47 -0200

    Criado um arquivo JS para conseguir validar o email criado como o do administrador

---
+++ Projeto_Site/js/logar.js
@@ -0,0 +1,11 @@
+function logar() {
+    if ((!form.email.value == 'admin.admin@gmail.com' && form.senha.value == '12345678') || (form.email.value == 'admin.admin@gmail.com' || form.senha.value == '12345678')) {
+
+        if (form.email.value == 'admin.admin@gmail.com' && form.senha.value == '12345678') {
+            return alert(`Seja bem vindo tricolor`);
+
+
+        }
+    }
+}
+
