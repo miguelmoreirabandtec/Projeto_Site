589ec9feceaf3903eaa00d2d8b1c83b702e3ec38
Matheus Gomes <hotmus8500@gmail.com>
2019-12-03 20:42:52 -0300

    Site venda concluido

--- Projeto_SIte/js/venda_ingressos.js
+++ Projeto_SIte/js/venda_ingressos.js
@@ -1,54 +1,33 @@
-var ingressos1 = [];
-var ingressos2 = [];
-var ingressos3 = [];
-var ingressos4 = [];
-
 function compra() {
+    var soma1 = Number(quantidade_ingressos1.value) * Number(50);
+    var soma2 = Number(quantidade_ingressos2.value) * Number(25);
+    var soma3 = Number(quantidade_ingressos3.value) * Number(50);
+    var soma4 = Number(quantidade_ingressos4.value) * Number(25);
+    // var somat = soma;
+    // var soma1t = soma1;
+    qnt.innerHTML = ` Setor Norte: ${soma1}<br>
+    Setor Norte-Meia: ${soma2}<br>
+    Setor Sul: ${soma3}<br>
+    Setor Sul-Meia: ${soma4}`;
 
-    var totalingressos1 = Number(quantidade_ingressos1.value);
-    var totalingressos2 = Number(quantidade_ingressos2.value);
-    var totalingressos3 = Number(quantidade_ingressos3.value);
-    var totalingressos4 = Number(quantidade_ingressos4.value);
+    console.log(soma1, soma2, soma3, soma4)
+    var total = Number(soma1 + soma2 + soma3 + soma4);
 
-
-
-    var soma = totalingressos1 + totalingressos2 + totalingressos3 + totalingressos4;
-    
-    {
-
-      
-
-        }
-         if (soma == 0) {
-            alert('Para efetuar uma compra,selecione um tipo de ingresso.')
-        } else {
-            ingressos1.push(totalingressos1);
-            ingressos2.push(totalingressos2);
-            ingressos3.push(totalingressos3);
-            ingressos4.push(totalingressos4);
-        }
-        for(var indice = 0; indice > soma; indice++){
-            var ticket = 50;
-            var ticket_meia = 25;
-
-            
-     }
-     function showRegisterForm() {
-        $('.loginBox').fadeOut('fast', function () {
-            $('.registerBox').fadeIn('fast');
-            $('.login-footer').fadeOut('fast', function () {
-                $('.register-footer').fadeIn('fast');
-            });
-            $('.modal-title').html('Registro');
-        });
-        $('.error').removeClass('alert alert-danger').html('');
-    
-    }
-
-    }
+    console.log('valor da soma: ', total)
+    qnt.innerHTML += `<br>Total: ${total}`
 
 
-    
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
--- Projeto_Site/js/logar.js
+++ Projeto_Site/js/logar.js
@@ -2,12 +2,11 @@ function logar() {
     if ((!email.value == 'admin.admin@gmail.com' && senha.value == '12345678') || (email.value == 'admin.admin@gmail.com' || senha.value == '12345678')) {
 
         if (email.value == 'admin.admin@gmail.com' && senha.value == '12345678') {
-             alert(`Seja bem vindo tricolor`);
             location.href = "./venda_ingressos.html";
 
 
         } else {
-            alert('E-mail ou senha inválidos');
+            alert1.innerHTML = 'E-mail ou senha inválidos';
         }
     }
 }
